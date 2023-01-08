960ee715bac6954b65030c9a3c098d0e3b62eed4
Miroslav Kravec <kravec.miroslav@gmail.com>
2023-01-08 12:14:48 +0100

    Add support for non-instanced tiling in project

--- internal/project/model.go
+++ internal/project/model.go
@@ -2,6 +2,14 @@ package project
 
 import "github.com/kravemir/labelset/tiling"
 
+type Project struct {
+	Datasets map[string]DataSet
+
+	Outputs Outputs
+
+	Archives []Archive
+}
+
 type JsonCollectionStorage struct {
 	Location string
 }
@@ -15,23 +23,30 @@ type DataSet struct {
 	CSVTableStorage       CSVTableStorage
 }
 
-type OutputSet struct {
+type Outputs struct {
+	Instancing map[string]InstancingSpecification
+	Tiling     map[string]TilingSpecification
+}
+
+type InstancingSpecification struct {
 	Template string
 	Dataset  string
 
-	AttributeRename map[string]string
-	OutputMimetypes []string
+	OutputTypes []string
 
 	Paper tiling.TiledPaper
 }
+
+type TilingSpecification struct {
+	Template string
+
+	OutputTypes []string
+
+	Paper tiling.TiledPaper
+}
+
 type Archive struct {
 	Name    string
 	Format  string
 	Sources []string
-}
-
-type Project struct {
-	Datasets   map[string]DataSet
-	OutputSets map[string]OutputSet
-	Archives   []Archive
 }
--- internal/project/runtime_makefile.go
+++ internal/project/runtime_makefile.go
@@ -26,10 +26,10 @@ func GenerateRuntimeMakefile(project Project) (string, error) {
 
 	makefile.append("\n")
 
-	for _, outputSetKey := range sortedKeys(project.OutputSets) {
-		outputSet := project.OutputSets[outputSetKey]
+	for _, outputSetKey := range sortedKeys(project.Outputs.Instancing) {
+		outputSet := project.Outputs.Instancing[outputSetKey]
 
-		for _, outputFormat := range outputSet.OutputMimetypes {
+		for _, outputFormat := range outputSet.OutputTypes {
 			makefile.appendVariable(
 				getOutputIdentifier(outputSetKey, outputFormat),
 				getOutputsGenerator(project, outputSetKey, outputSet, outputFormat),
@@ -37,45 +37,88 @@ func GenerateRuntimeMakefile(project Project) (string, error) {
 		}
 	}
 
-	for _, outputSetKey := range sortedKeys(project.OutputSets) {
-		outputSet := project.OutputSets[outputSetKey]
+	for _, outputSetKey := range sortedKeys(project.Outputs.Instancing) {
+		outputSet := project.Outputs.Instancing[outputSetKey]
 
 		makefile.appendVariable(
 			getOutputSetIdentifier(outputSetKey)+"_PAPER",
-			buildPaperOptions(outputSet),
+			buildPaperOptions(outputSet.Paper),
+		)
+	}
+	for _, tilingKey := range sortedKeys(project.Outputs.Tiling) {
+		tilingSpecification := project.Outputs.Tiling[tilingKey]
+
+		makefile.appendVariable(
+			getOutputTilingIdentifier(tilingKey)+"_PAPER",
+			buildPaperOptions(tilingSpecification.Paper),
 		)
 	}
 
 	makefile.append("\n")
 
 	var outputTargets []string
-	for _, outputSetKey := range sortedKeys(project.OutputSets) {
-		outputSet := project.OutputSets[outputSetKey]
+	for _, outputSetKey := range sortedKeys(project.Outputs.Instancing) {
+		outputSet := project.Outputs.Instancing[outputSetKey]
 
-		for _, outputFormats := range outputSet.OutputMimetypes {
+		for _, outputFormats := range outputSet.OutputTypes {
 			outputTargets = append(outputTargets, getOutputVariable(outputSetKey, outputFormats))
+		}
+	}
+	for _, tilingKey := range sortedKeys(project.Outputs.Tiling) {
+		tilingSpecification := project.Outputs.Tiling[tilingKey]
+
+		for _, outputType := range tilingSpecification.OutputTypes {
+			outputTargets = append(outputTargets, getTilingLocation(tilingKey, outputType))
 		}
 	}
 	makefile.appendRule("all", strings.Join(outputTargets, " "))
 
-	for _, outputSetKey := range sortedKeys(project.OutputSets) {
-		outputSet := project.OutputSets[outputSetKey]
+	for _, outputSetKey := range sortedKeys(project.Outputs.Instancing) {
+		outputSet := project.Outputs.Instancing[outputSetKey]
 
 		svgOutputRule := "${TMP_DIR}/" + outputSetKey + "/%.svg"
 
-		if slices.Contains(outputSet.OutputMimetypes, "svg") {
+		if slices.Contains(outputSet.OutputTypes, "svg") {
 			svgOutputRule = "${OUT_DIR}/" + outputSetKey + "/%.svg"
 		}
 
 		generateSvgRule(&makefile, project, outputSetKey, outputSet, svgOutputRule)
 
-		for _, outputFormat := range outputSet.OutputMimetypes {
+		for _, outputFormat := range outputSet.OutputTypes {
 			switch strings.ToLower(outputFormat) {
 			case "svg":
 				break
 			case "pdf":
 				makefile.appendRule(
 					"${OUT_DIR}/"+outputSetKey+"/%.pdf",
+					svgOutputRule,
+					`inkscape "$<" --export-type="pdf" --export-filename="$@"`,
+				)
+				break
+			default:
+				panic(fmt.Errorf("unsupported format %s", outputFormat))
+			}
+		}
+	}
+
+	for _, tilingKey := range sortedKeys(project.Outputs.Tiling) {
+		tilingSpecification := project.Outputs.Tiling[tilingKey]
+
+		svgOutputRule := "${TMP_DIR}/" + tilingKey + ".svg"
+
+		if slices.Contains(tilingSpecification.OutputTypes, "svg") {
+			svgOutputRule = "${OUT_DIR}/" + tilingKey + ".svg"
+		}
+
+		generateTilingSvgRule(&makefile, tilingKey, tilingSpecification, svgOutputRule)
+
+		for _, outputFormat := range tilingSpecification.OutputTypes {
+			switch strings.ToLower(outputFormat) {
+			case "svg":
+				break
+			case "pdf":
+				makefile.appendRule(
+					"${OUT_DIR}/"+tilingKey+".pdf",
 					svgOutputRule,
 					`inkscape "$<" --export-type="pdf" --export-filename="$@"`,
 				)
@@ -113,7 +156,7 @@ func GenerateRuntimeMakefile(project Project) (string, error) {
 	makefile.appendMkdirRule("${OUT_DIR}")
 	makefile.appendMkdirRule("archives")
 
-	for _, outputSetKey := range sortedKeys(project.OutputSets) {
+	for _, outputSetKey := range sortedKeys(project.Outputs.Instancing) {
 		makefile.appendMkdirRule("${OUT_DIR}/" + outputSetKey)
 		makefile.appendMkdirRule("${TMP_DIR}/" + outputSetKey)
 	}
@@ -128,7 +171,11 @@ func GenerateRuntimeMakefile(project Project) (string, error) {
 
 }
 
-func generateSvgRule(makefile *makefileBuilder, project Project, outputSetKey string, outputSet OutputSet, svgOutputRule string) {
+func getTilingLocation(tilingKey string, outputType string) string {
+	return fmt.Sprintf("${OUT_DIR}/%s.%s", tilingKey, strings.ToLower(outputType))
+}
+
+func generateSvgRule(makefile *makefileBuilder, project Project, outputSetKey string, outputSet InstancingSpecification, svgOutputRule string) {
 	srcDataSet := project.Datasets[outputSet.Dataset]
 
 	var srcFile string
@@ -154,16 +201,26 @@ func generateSvgRule(makefile *makefileBuilder, project Project, outputSetKey st
 	)
 }
 
+func generateTilingSvgRule(makefile *makefileBuilder, tilingKey string, tilingSpecification TilingSpecification, svgOutputRule string) {
+	var options strings.Builder
+
+	options.WriteString("$(" + getOutputTilingIdentifier(tilingKey) + "_PAPER) ")
+
+	makefile.appendRule(
+		svgOutputRule,
+		tilingSpecification.Template+" | ${OUT_DIR}",
+		"$(LABELSET_BIN) tile "+options.String()+tilingSpecification.Template+" \"$@\"",
+	)
+}
+
 func getArchiveLocation(archive Archive) string {
 	return fmt.Sprintf("archives/%s.%s", archive.Name, archive.Format)
 }
 
-func buildPaperOptions(set OutputSet) string {
-	if set.Paper == (tiling.TiledPaper{}) {
+func buildPaperOptions(paper tiling.TiledPaper) string {
+	if paper == (tiling.TiledPaper{}) {
 		panic("Paper definition expected")
 	}
-
-	paper := set.Paper
 
 	return fmt.Sprintf(
 		`--paper-size "%.3fx%.3f" --label-offset "%.3f,%.3f" --label-size "%.3fx%.3f" --label-spacing "%.3f,%.3f"`,
@@ -174,17 +231,17 @@ func buildPaperOptions(set OutputSet) string {
 	)
 }
 
-func genInfoCommands(result Project) []string {
+func genInfoCommands(project Project) []string {
 	var commands []string
 
-	for _, dataSetKey := range sortedKeys(result.Datasets) {
+	for _, dataSetKey := range sortedKeys(project.Datasets) {
 		commands = append(commands, fmt.Sprintf("@echo DataSet %s items: $(%s)", dataSetKey, getDatasetIdentifier(dataSetKey)))
 	}
 
-	for _, outputSetKey := range sortedKeys(result.OutputSets) {
-		outputSet := result.OutputSets[outputSetKey]
+	for _, outputSetKey := range sortedKeys(project.Outputs.Instancing) {
+		outputSet := project.Outputs.Instancing[outputSetKey]
 
-		for _, outputFormat := range outputSet.OutputMimetypes {
+		for _, outputFormat := range outputSet.OutputTypes {
 			commands = append(commands,
 				fmt.Sprintf(
 					"@echo Outputs %s, %s items: $(%s)",
@@ -208,7 +265,7 @@ func genImmediateItemsVariable(makefile *makefileBuilder, dataSetKey string, dat
 	}
 }
 
-func getOutputsGenerator(project Project, outputSetKey string, outputSet OutputSet, outputFormet string) string {
+func getOutputsGenerator(project Project, outputSetKey string, outputSet InstancingSpecification, outputFormet string) string {
 	dataset := project.Datasets[outputSet.Dataset]
 
 	if dataset.JsonCollectionStorage != (JsonCollectionStorage{}) {
@@ -247,6 +304,10 @@ func getOutputIdentifier(outputSetKey string, outputFormat string) string {
 
 func getOutputSetIdentifier(outputSetKey string) string {
 	return fmt.Sprintf("OUTPUTS_%s", outputSetKey)
+}
+
+func getOutputTilingIdentifier(tilingKey string) string {
+	return fmt.Sprintf("OUTPUTS_tiling_%s", tilingKey)
 }
 
 func sortedKeys[M ~map[string]V, V any](m M) []string {
